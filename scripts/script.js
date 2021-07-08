0850bd2c8233f2ebf9cc769df5ac5bd3ac6cc2fd
Lev Karpov <nt66@bk.ru>
2021-07-08 12:50:05 +0300

    Заполнение страницы товара

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,31 @@ const goodsTitle = document.querySelector('.goods__title');
 
 let hash = location.hash.substring(1);
 
-headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город';
+const updateLocation = () => {
+  headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город';
+}
 
 headerCityButton.addEventListener('click', () => {
   /*prompt - модальное окно встроенное в браузер*/
-    const city = prompt('Укажите ваш город');
-    headerCityButton.textContent = city;
-    localStorage.setItem('lomoda-location', city);
+    const city = prompt('Укажите ваш город').trim();
+    if(city !== null) {
+      localStorage.setItem('lomoda-location', city);
+    }
+    updateLocation();
 });
+
+updateLocation();
 
 /*Блокировка скролла*/
 const disableScroll = () => {
+
+  if(document.disableScroll) return;
+
   const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+  document.disableScroll = true;
   document.body.dbScrollY = window.scrollY;
+
   document.body.style.cssText = `
      position: fixed;
      top: ${-window.scrollY}px;
@@ -29,6 +41,8 @@ const disableScroll = () => {
 };
 
 const enableScroll = () => {
+  document.disableScroll = false;
+
   document.body.style.cssText = '';
   window.scroll({
     top: document.body.dbScrollY,
@@ -61,11 +75,11 @@ const getData = async () => {
   }
 };
 
-const getGoods = (callback, value) => {
+const getGoods = (callback, value, prop) => {
   getData()
     .then(data => {
       if (value) {
-        callback(data.filter(item => item.category === value));
+        callback(data.filter(item => item[prop] === value));
       } else {
         callback(data);
       }
@@ -128,14 +142,81 @@ try {
 
    window.addEventListener('hashchange', (event) => {
     hash = location.hash.substring(1);
-    getGoods(renderGoodsList, hash);
+    getGoods(renderGoodsList, hash, 'category');
   });
 
-   getGoods(renderGoodsList, hash);
+   getGoods(renderGoodsList, hash, 'category');
 
 } catch (err) {
     console.warn(err);
 };
+
+// страницы товаров
+
+try {
+
+  if (!document.querySelector('.card-good')) {
+       throw 'This is not a card-good page' 
+  }
+
+      const cardGoodImage = document.querySelector('.card-good__image');
+      const cardGoodBrand = document.querySelector('.card-good__brand');
+      const cardGoodTitle = document.querySelector('.card-good__title');
+      const cardGoodPrice = document.querySelector('.card-good__price');
+      const cardGoodColor = document.querySelector('.card-good__color');
+      const cardGoodColorList = document.querySelector('.card-good__color-list');
+      const cardGoodSizes = document.querySelector('.card-good__sizes');
+      const cardGoodSizesList = document.querySelector('.card-good__sizes-list');
+      const cardGoodSelectWrapper = document.querySelectorAll('.card-good__select__wrapper');
+      const cardGoodBuy = document.querySelector('.card-good__buy');
+
+      const generateList = data => data.reduce((html, item, i) =>
+        html + `<li class="card-good__select-item" data-id="${i}">${item}</li>`, '');
+
+      const renderCardGood = ([{ brand, name, cost, color, sizes, photo }]) => {
+        cardGoodImage.src = `goods-image/${photo}`;
+        cardGoodImage.alt = `${brand} + ${name}`;
+        cardGoodBrand.textContent = brand;
+        cardGoodTitle.textContent = name;
+        cardGoodPrice.textContent = `${cost} ₽`;
+        if (color) {
+          cardGoodColor.textContent = color[0];
+          cardGoodColor.dataset.id = 0;
+          cardGoodColorList.innerHTML = generateList(color);
+        } else {
+          cardGoodColor.style.display = 'none'
+        };
+
+        if (sizes) {
+          cardGoodSizes.textContent = sizes[0];
+          cardGoodSizes.dataset.id = 0;
+          cardGoodSizesList.innerHTML = generateList(sizes);
+        } else {
+          cardGoodSizes.style.display = 'none'
+        }
+      }
+
+      cardGoodSelectWrapper.forEach (item => {
+          item.addEventListener ('click', e => {
+              const target = e.target;
+
+              if (target.closest('.card-good__select')) {
+                target.classList.toggle('card-good__select__open');
+              }
+
+              if (target.closest('.card-good__select-item')) {
+                const cardGoodSelect = item.querySelector('.card-good__select');
+                cardGoodSelect.textContent = target.textContent;
+                cardGoodSelect.dataset.id = target.dataset.id;
+                cardGoodSelect.classList.remove('card-good__select__open');
+              }
+          });
+      });
+      getGoods (renderCardGood, hash, 'id');
+
+} catch (err) {
+  console.warn(err)
+}
 
 //eventListnerы (слушатели)
 subheaderCart.addEventListener('click', cartModalOpen);
@@ -151,5 +232,5 @@ cartOverlay.addEventListener('click', event => {
 navigationLink.forEach (item => {
   item.addEventListener('click', () => {
     goodsTitle.textContent = item.textContent; 
-  })
+  });
 });
